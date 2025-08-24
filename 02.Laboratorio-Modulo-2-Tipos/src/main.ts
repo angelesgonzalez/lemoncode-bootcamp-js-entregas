d459921be8c041a9266f2c8bcae674ae48ad8126
Angeles <delosangelesgonzalez@gmail.com>
2025-08-24 13:37:02 +0200

    Complete module 2 exercise

--- 02.Laboratorio-Modulo-2-Tipos/src/main.ts
+++ 02.Laboratorio-Modulo-2-Tipos/src/main.ts
@@ -1,3 +1,18 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+const ticket = {
+	total: 120,
+	drinks: 18,
+	friends: 6,
+	totalPeople: 6 + 1,
+};
+
+const totalToPay = Number(
+	((ticket.total - ticket.drinks) / ticket.totalPeople).toFixed(2)
+);
+
+const birthdayTotal = Number((totalToPay + ticket.drinks).toFixed(2));
+
+console.log(
+	`Each friend has to pay ${totalToPay}. The birthday person pays: ${birthdayTotal}`
+);
